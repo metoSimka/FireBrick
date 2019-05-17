556d54c010511334892ad07d26ac540d72bf02a8
MAKS <SimkaWitch@gmail.com>
2019-05-17 09:43:26 +0600

    Split to multiple story boards, code style fixes

--- FireBrick/UI/E-mail/EmailSignUpViewController.swift
+++ FireBrick/UI/E-mail/EmailSignUpViewController.swift
@@ -18,7 +18,7 @@ class EmailSignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    emailTextField.delegate = self
+        emailTextField.delegate = self
         passwordTextField.delegate = self
 
     }
@@ -27,36 +27,23 @@ class EmailSignUpViewController: UIViewController {
         
         if emailTextField.text == "" {
             let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
-            
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
-            
             present(alertController, animated: true, completion: nil)
-            
         } else {
             Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-                
                 if error == nil {
                     print("You have successfully signed up")
-
                     let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
                     self.present(vc!, animated: true, completion: nil)
-                    
                 } else {
                     let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
-                    
                     self.present(alertController, animated: true, completion: nil)
                 }
             }
         }
-        
-    }
-    
-    @IBAction func login(_ sender: UIButton) {
-        
     }
 }
 
--- FireBrick/UI/E-mail/LoginEmailViewController.swift
+++ FireBrick/UI/E-mail/LoginEmailViewController.swift
@@ -21,40 +21,23 @@ class LoginEmailViewController: UIViewController {
         passwordTextField.delegate = self
     }
     
-    
     @IBAction func login(_ sender: UIButton) {
         if self.emailTextField.text == "" || self.passwordTextField.text == "" {
-            
-            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
-            
             let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
-            
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
-            
             self.present(alertController, animated: true, completion: nil)
-            
         } else {
             
             Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
-                
                 if error == nil {
-                    
-                    //Print into the console if successfully logged in
                     print("You have successfully logged in")
-                    
-                    //Go to the HomeViewController if the login is sucessful
                     let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
                     self.present(vc!, animated: true, completion: nil)
-                    
                 } else {
-                    
-                    //Tells the user that there is an error and then gets firebase to tell them the error
                     let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    
+                    alertController.addAction(defaultAction)                  
                     self.present(alertController, animated: true, completion: nil)
                 }
             }
